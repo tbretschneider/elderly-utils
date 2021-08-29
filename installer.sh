7c5a77b5c255b402894d0e4476fa97f169bb2915
Tobias Bretschneider <tobias@ehgraphicdesign.co.uk>
2021-08-29 20:46:32 +0100

    new file:   installer.sh 	modified:   pnms2pdf 	image.pnm 	test.pdf

---
+++ installer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo ln -s attempt2.sh /usr/bin/attempt2.sh
+sudo ln -s confirmation1.sh /usr/bin/confirmation1.sh
+sudo ln -s dashes.sh /usr/bin/dashes.sh
+sudo ln -s makenewfolder.sh /usr/bin/makenewfolder.sh
+sudo ln -s newfile.sh /usr/bin/newfile.sh
+sudo ln -s pnms2pdf /usr/bin/pnms2pdf
+sudo ln -s scanprogram.sh /usr/bin/scanprogram.sh
+sudo ln -s selectfolder.sh /usr/bin/selectfolder.sh
